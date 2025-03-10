fe8035641296f1dfba6ae77c813fb2efd3249ba0
sendlan <1095546891@qq.com>
2025-03-10 18:07:09 +0800

    fix clash core

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -20,8 +20,8 @@
 sed -i 's/192.168.110.1/192.168.2.1/g' $(find feeds/luci/modules/luci-mod-system -type f -name "flash.js")
 #修改默认IP地址.1/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/blob/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar -zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash >/dev/null 2>&1
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
